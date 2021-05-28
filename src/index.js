fad47a5a1c16d475e773ebe4145d9d424b88c52c
Nhi Nguyen <nguyentuyetnhi107109@gmail.com>
2021-05-29 02:12:48 +0700

    add yarn

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import 'antd/dist/antd.less'
 import App from './App'
-import store from './app/store'
-import { Provider } from 'react-redux'
 import * as serviceWorker from './serviceWorker'
 
 import { ThemeProvider } from 'styled-components'
