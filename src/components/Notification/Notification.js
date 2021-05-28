04f02478c4511cba498ff352eed311102f66b16d
Nhi Nguyen <nguyentuyetnhi107109@gmail.com>
2021-05-29 02:30:28 +0700

    cut some thing is not necessary

--- src/components/Notification/Notification.js
+++
@@ -1,9 +0,0 @@
-import { notification } from 'antd'
-
-export const Notification = ({ type, message, description }) => {
-  notification[type]({
-    message,
-    description,
-    placement: 'bottomRight'
-  })
-}
