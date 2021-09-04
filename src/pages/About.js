27ccc3084a8790ed5f92d3d228c111e6539ad8a5
nemo <huynhviha1703@gmail.com>
2021-09-04 16:01:15 +0700

    about us page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,44 +1,70 @@
 import React from 'react'
-import { ImgAboutCover, BlogsCover } from '../style/pages.style'
-import { AuthorBlock } from '../style/pages.style'
+import {HeaderBlock, HeaderContentCover, NavigatorCover, OnlyCenter, TitleBlock} from '../components/Block/HeaderBlock';
+import {Image, Row, Col} from 'antd';
+import MyNavigation from '../components/Navigation/Navigation';
+import SearchCustom from '../components/Search/Search';
+import {AuthorBlock, AuthorPicture} from '../components/Author/AuthorBlock';
+import {TextLarge, TextSmall, Title} from '../components/Text/Text';
+import {LineOutlined} from '@ant-design/icons';
+import Footer from "../components/Footer/Footer";
 
 const About = () => {
     return (
-        <BlogsCover>
-            <AuthorBlock class="hentry">
-                <header class="entry-header">
-                    <h1 class="entry-title">Zelda</h1>
-                </header>
+        <>
+            <HeaderBlock>
+                <NavigatorCover>
+                    <Image
+                        width={200}
+                        src='mylogo.svg'
+                        preview={{
+                            src: 'mylogo.svg'
+                        }}
+                    />
+                    <MyNavigation id='my-navigator' />
+                </NavigatorCover>
+                <HeaderContentCover>
+                    <SearchCustom />
+                    <div>
+                        <Title>
+                            HAI NGƯỜI ĐI
+                            KHẮP THẾ GIAN
+                        </Title>
+                        <TextSmall>
+                            THE ONLY TRIP YOU WILL REGRET
+                            IS THE ONE YOU DON’T TAKE
+                        </TextSmall>
+                    </div>
 
-
-                <div class="entry-content">
-                    <ImgAboutCover>
-                        <img src='./aboutzelda.png' alt="bg5" class="alignright" />
-
-                    </ImgAboutCover>
-                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-                    <p>It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-                    {/* <h2 style="font-family: 'Herr Von Muellerhoff';color:#ccc;font-weight:300;">Yours, Von Muellerhoff</h2> */}
+                </HeaderContentCover>
+            </HeaderBlock>
+            <TitleBlock>
+                <div style={{ lineHeight: '0' }}>
+                    <TextLarge style={{ color: 'black' }}>ABOUT US</TextLarge>
+                    <OnlyCenter>
+                        <LineOutlined style={{ color: 'black', fontSize: '50px' }} />
+                    </OnlyCenter>
                 </div>
-            </AuthorBlock>
-            <AuthorBlock class="hentry">
-                <header class="entry-header">
-                    <h1 class="entry-title">Tê Tê</h1>
-                </header>
-
-
-                <div class="entry-content">
-                    <ImgAboutCover>
-                        <img src='./abouttete.png' alt="bg5" class="alignright" />
-
-                    </ImgAboutCover>
-                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-                    <p>It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-                    {/* <h2 style="font-family: 'Herr Von Muellerhoff';color:#ccc;font-weight:300;">Yours, Von Muellerhoff</h2> */}
-                </div>
-            </AuthorBlock>
-        </BlogsCover>
-
+            </TitleBlock>
+            <div style={{background: '#F8F7F3'}}>
+                <Row>
+                    <Col span={8}>
+                        <AuthorPicture side="right" bgSrc="zelda-avatar-background.svg" src="zelda-profile-picture.png"/>
+                    </Col>
+                    <Col span={16}>
+                        <AuthorBlock name="ZELDA" description="But art, true art that is, truly does have the capability of bringing us all together and revealing to ourselves our true nature as human beings. We are driven to create and appreciate great pieces of music, films and paintings even those that are apolitical and seemingly irrelevant to the on-going events - out of boredom, and the ever desire to express our thoughts and emotions"/>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col span={16}>
+                        <AuthorBlock name="NEMO" description="But art, true art that is, truly does have the capability of bringing us all together and revealing to ourselves our true nature as human beings. We are driven to create and appreciate great pieces of music, films and paintings even those that are apolitical and seemingly irrelevant to the on-going events - out of boredom, and the ever desire to express our thoughts and emotions"/>
+                    </Col>
+                    <Col span={8}>
+                        <AuthorPicture side="left" bgSrc="nemo-avatar-background.svg" src="nemo-profile-picture.png"/>
+                    </Col>
+                </Row>
+            </div>
+            <Footer />
+        </>
     )
 }
 
